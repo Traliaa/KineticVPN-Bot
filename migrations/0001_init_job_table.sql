ba92bb64ffa47a690cfb7e57c7d585d7fe3d77e4
Aleksey Korastelev <akorastelev@ozon.ru>
2025-10-01 18:04:45 +0300

    init app

--- migrations/0001_init_job_table.sql
+++ migrations/0001_init_job_table.sql
@@ -1,14 +1,22 @@
+
 -- +goose Up
 -- +goose StatementBegin
+
 CREATE TABLE user_settings (
-                          id bigserial PRIMARY KEY,
-                          chatID bigint NOT NULL,
-                          name text NOT NULL,
-                          router_url text,
-                          username text,
-                          password text,
-                          SelectedApps text[],
-                          auth_code text,
-                          step text
+                               id bigserial PRIMARY KEY,
+                               chatID bigint NOT NULL,
+                               name text NOT NULL,
+                               router_url text,
+                               username text,
+                               password text,
+                               SelectedApps text[],
+                               auth_code text,
+                               step text
 
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+-- +goose StatementEnd
